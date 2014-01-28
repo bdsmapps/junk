d8aad6f21ca8c30267d7194811c00b536555fd6d
Admin <admin@Admins-Mac.local>
2014-01-28 19:29:46 +0200

    qwerty5

---
+++ mpcremote/mpcremote/MPCRHelpView.h
@@ -0,0 +1,13 @@
+//
+//  MPCRHelpView.h
+//  mpcremote
+//
+//  Created by Admin on 1/28/14.
+//  Copyright (c) 2014 Bohdan Likhyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPCRHelpView : UIViewController
+
+@end
---
+++ mpcremote/mpcremote/MPCRHelpView.m
@@ -0,0 +1,38 @@
+//
+//  MPCRHelpView.m
+//  mpcremote
+//
+//  Created by Admin on 1/28/14.
+//  Copyright (c) 2014 Bohdan Likhyy. All rights reserved.
+//
+
+#import "MPCRHelpView.h"
+
+@interface MPCRHelpView ()
+
+@end
+
+@implementation MPCRHelpView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
