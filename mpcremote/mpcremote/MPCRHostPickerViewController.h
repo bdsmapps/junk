c543f979844b18c5a10ac7a3769ed7737a962864
Admin <admin@Admins-Mac.local>
2014-01-28 15:59:22 +0200

    qwerty4

---
+++ mpcremote/mpcremote/MPCRHostPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPCRHostPickerViewController.h
+//  mpcremote
+//
+//  Created by Admin on 1/28/14.
+//  Copyright (c) 2014 Bohdan Likhyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPCRHostPickerViewController : UITableViewController
+
+@end
